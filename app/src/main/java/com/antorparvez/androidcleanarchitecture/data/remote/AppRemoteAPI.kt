37ca579b4cdc3bc01f907bb30c607e0eaaaee1b9
antor <antorparvez@gmail.com>
2022-03-31 17:58:00 +0600

    Hilt and Retrofit Implemented

--- app/src/main/java/com/antorparvez/androidcleanarchitecture/data/remote/AppRemoteAPI.kt
+++ app/src/main/java/com/antorparvez/androidcleanarchitecture/data/remote/AppRemoteAPI.kt
@@ -1,15 +1,16 @@
 package com.antorparvez.androidcleanarchitecture.data.remote
 
 import com.antorparvez.androidcleanarchitecture.data.dto_model.MealDTO
+import com.antorparvez.androidcleanarchitecture.data.dto_model.MealsDTO
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface AppRemoteAPI {
 
     @GET("/search.php")
-    suspend fun getMealList(@Query("s") s:String) : MealDTO
+    suspend fun getMealList(@Query("s") s:String) : MealsDTO
 
     @GET("/lookup.pnp")
-    suspend fun getMealDetails(@Query("i") i:String):MealDTO
+    suspend fun getMealDetails(@Query("i") i:String):MealsDTO
 
 }
--- app/src/main/java/com/antorparvez/androidcleanarchitecture/data/repository_imp/DataRepository.kt
+++ app/src/main/java/com/antorparvez/androidcleanarchitecture/data/repository_imp/DataRepository.kt
@@ -1,15 +1,15 @@
 package com.antorparvez.androidcleanarchitecture.data.repository_imp
 
-import com.antorparvez.androidcleanarchitecture.data.dto_model.MealDTO
+import com.antorparvez.androidcleanarchitecture.data.dto_model.MealsDTO
 import com.antorparvez.androidcleanarchitecture.data.remote.AppRemoteAPI
 import com.antorparvez.androidcleanarchitecture.domain.repository.ApiRepository
 
 class DataRepository(private val api: AppRemoteAPI) : ApiRepository{
-    override suspend fun getMealList(s: String): MealDTO {
+    override suspend fun getMealList(s: String): MealsDTO {
        return api.getMealList(s)
     }
 
-    override suspend fun getMealDetails(s: String): MealDTO {
+    override suspend fun getMealDetails(s: String): MealsDTO {
        return api.getMealDetails(s)
     }
 }
